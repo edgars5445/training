81caf6b2d84999cc4a6dddf4a560fc8cb24c93cf
edgars5445 <pastorsedgars5445@gmail.com>
2022-01-07 20:03:04 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <head>
+        <title>Training page</title>
+        <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
+    </head>
+    <div class="main-block">
+        <ul>
+            <li>Tehnika</li>
+            <li>Elektronika</li>
+            <li>Mājas</li>
+            <li>Internets</li>
+            <li>Gaisma</li>
+        </ul>
+    </div>
+</x-app-layout>
